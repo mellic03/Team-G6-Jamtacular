f5981bdbbadbf4c8e6deb0444d10b08df7414e28
Michael Ellicott <47673882+mellic03@users.noreply.github.com>
2021-10-31 12:22:40 +1000

    Map and player class base

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
 
-function preload() {
+let map;
+let mapFile;
+let wall;
 
+let player;
+
+function preload() {
+    mapFile = loadImage("assets/tileMap.png");
+    wall = loadImage("assets/tile.png");
 }
 
 
 function setup() {
-    createCanvas(1000, 1000);
+    createCanvas(800, 600);
     frameRate(60);
+    
+    map = new Map();
+    map.generate(mapFile, wall);
 
+    player = new Player();
+    player.generate(100, 100);
 }
-
+ 
 
 function draw() {
-    background(200);
+    background(0);
+    
+    drawSprites();
+
+    map.draw();
+    player.control();
+    player.sprite.collide(map.blocks);
 
 }
