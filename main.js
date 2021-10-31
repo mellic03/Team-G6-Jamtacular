101df29ab52a16c838612d1f5feb14ca4974b5d3
Michael Ellicott <47673882+mellic03@users.noreply.github.com>
2021-10-31 22:39:20 +1000

    Player logic and map addition
    Map can now read pixel values of 200 and create doors.

--- main.js
+++ main.js
@@ -2,13 +2,25 @@
 let map;
 let mapFile;
 let wall;
+let door;
 
 let player;
+let playerIdle;
+let playerWalk;
 
+let idleAnim;
+let walkAnim;
 
 function preload() {
     mapFile = loadImage("assets/tileMap.png");
     wall = loadImage("assets/tile.png");
+    door = loadImage("assets/door.png");
+
+    playerIdle = loadSpriteSheet("assets/playerIdle.png", 64, 64, 1);
+    playerWalk = loadSpriteSheet("assets/playerWalk.png", 64, 64, 10);
+    idleAnim = loadAnimation(playerIdle);
+    walkAnim = loadAnimation(playerWalk);
+
 }
 
 
@@ -17,21 +29,26 @@ function setup() {
     frameRate(60);
     
     map = new Map();
-    map.generate(mapFile, wall, 0, -200);
+    map.generate(mapFile, wall, door, 0, -200);
 
     player = new Player();
-    player.generate(100, 100);
+    player.generate(1200, 0);
 
 }
  
 
 function draw() {
     background(0);
+
     drawSprite(player.sprite);
-
-
+    drawSprites();
+    
     map.draw();
     player.control();
     player.sprite.collide(map.blocks);
 
+}
+
+function keyPressed() {
+    //requestPointerLock();
 }
