b845689c6f37982bcbb311b9888b9f942c52cbd2
Michael Ellicott <47673882+mellic03@users.noreply.github.com>
2021-11-02 12:27:48 +1000

    big update

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 
+let rayCastOn = false;
+
 let map;
 let mapFile;
 let wall;
+let brickImg;
 let door;
 
 let player;
 let playerIdle;
 let playerWalk;
+
+let pistol;
 
 let idleAnim;
 let walkAnim;
@@ -16,27 +21,26 @@ let rayCast;
 
 
 function preload() {
-    mapFile = loadImage("assets/tileMap.png");
-    wall = loadImage("assets/tile.png");
-    door = loadImage("assets/door.png");
+    mapFile = loadImage("assets/img/map/tileMap.png");
+    wall = loadImage("assets/img/map/tile.png");
+    brickImg = loadImage("assets/img/map/brick.png");
+    door = loadImage("assets/img/map/door.png");
 
-    playerIdle = loadSpriteSheet("assets/playerIdle.png", 64, 64, 1);
-    playerWalk = loadSpriteSheet("assets/playerWalk.png", 64, 64, 10);
+    playerIdle = loadSpriteSheet("assets/img/player/playerIdle.png", 64, 64, 1);
+    playerWalk = loadSpriteSheet("assets/img/player/playerWalk.png", 64, 64, 10);
+    pistol = loadImage("assets/img/weapon/pistol.png");
     idleAnim = loadAnimation(playerIdle);
     walkAnim = loadAnimation(playerWalk);
-
 }
 
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(800, 600);
     frameRate(60);
     
     map = new Map("");  // change to map = new Map("raycasted") for raycasting
-    map.generate(mapFile, wall, door, 0, -200);
-
-    player = new Player();
-    player.generate(1200, 100);
+    map.generate(mapFile, wall, brickImg, door, 0, -200);
+    player = new Player(1200, 100);
 
     ui = new UI(player);    // pass in player object into ui class
 }
@@ -45,13 +49,12 @@ function setup() {
 function draw() {
     background(0);
 
-    map.draw();
-
-    drawSprite(player.sprite);
-    
     player.control();
     player.sprite.collide(map.blocks);
-
+    drawSprites();
     // draws ui box
     ui.draw(50, 50);
+
+
+
 }
